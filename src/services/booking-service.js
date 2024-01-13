8d5d60e9a8b4ebdddfbe64ad50a5aee2c9d60079
amaanraaj <amandwivedi00@gmail.com>
2024-01-13 23:28:44 +0530

    working on create booking api

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -1,3 +1,24 @@
+const axios = require('axios');
+const { BookingRepository } = require('../repositories');
+const db = require('../models');
+const { FLIGHT_SERVICE } = require('../config/server-config');
+
+const bookingRepository = new BookingRepository();
+
+async function createBooking(data){
+    console.log(data);
+    try {
+        const result = db.sequelize.transaction(async function bookingImpl(t){
+            const flight = await axios.get(`${FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`);
+            console.log(flight.data);
+            return true;
+        })
+    } catch (error) {
+        
+    }
+}
+
 module.exports = {
+    createBooking
     
 }
